9bf169023105e3293a5987289fe9ad34967679e1
Zhenbo Li <litimetal@gmail.com>
2021-08-10 16:46:27 +0800

    next
    Signed-off-by: Zhenbo Li <litimetal@gmail.com>

--- auto_release.py
+++ auto_release.py
@@ -2,6 +2,7 @@
 import subprocess
 MESSAGE_FLAG = "BOT MESSAGE: AUTO BUMP VERSION"
 
+
 def shell_call(cmd:str):
     s = subprocess.run(cmd, shell=True, capture_output=True).stdout
     return s.decode("utf-8").strip()
@@ -9,7 +10,7 @@ def shell_call(cmd:str):
 
 def main():
     branch_name = shell_call("git branch --show-current")
-    if branch_name not in ['master'] or True:
+    if branch_name not in ['master'] and branch_name != 'bump':
         print("Current branch  ({})  is not for release. Exiting".format(branch_name))
     commit_log = shell_call("git log --name-status -1")
     if MESSAGE_FLAG in commit_log:
