0e516dba72dcaa8a5e06f6f5d3b468e9d78f30e4
Zhenbo Li <3221521+Endle@users.noreply.github.com>
2021-08-13 17:29:26 +0800

    Better cli test (#15)
    Signed-off-by: Zhenbo Li <litimetal@gmail.com>

--- tests/bin.rs
+++ tests/bin.rs
@@ -4,21 +4,32 @@ use assert_cli::Assert;
 
 #[test]
 fn usage() {
-    Assert::main_binary()
-        .with_args(&[] as &[&str])
-        .fails()
-        .stderr()
-        .contains("Error! Input path have to be specified")
-        .unwrap();
+    cli_bundle(&[],
+               false,
+               &["Error! Input path have to be specified"])
 }
 
 #[test]
 fn bundle_self() {
-    Assert::main_binary()
-        .with_args(&["--input", "."])
-        .stdout()
-        .contains("pub fn bundle<")
-        .stdout()
-        .contains("let code = bundle(")
-        .unwrap();
+    cli_bundle(&["--input", "."],
+               true,
+               &["pub fn bundle<", "let code = bundle("])
+
+}
+
+fn cli_bundle(args: &[&str], success: bool, expect_output: &[&str]) -> () {
+    let mut assert = Assert::main_binary().with_args(args);
+    if success {
+        assert = assert.succeeds();
+    }  else {
+        assert = assert.fails();
+    }
+    for s in expect_output {
+        let ss = *s;
+        if success {
+            assert = assert.stdout().contains(ss);
+        } else {
+            assert = assert.stderr().contains(ss);
+        }
+    }
 }
